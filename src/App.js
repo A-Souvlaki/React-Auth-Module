8e6124d871d5c91b0bcc9a2e19df43c1a9493b22
souvlaki <1010144023@u.icesi.edu.co>
2022-04-24 18:09:00 -0500

    simple auth

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Router, Switch, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Login from "./components/login/Login";
+import Register from "./components/login/Register";
+import Profile from "./components/login/Profile";
+import BoardUser from "./components/login/BoardUser";
+import BoardModerator from "./components/login/BoardModerator";
+import BoardAdmin from "./components/login/BoardAdmin";
+import { BsHeartFill } from "react-icons/bs";
+import { logout } from "./actions/auth";
+import { clearMessage } from "./actions/message";
+import { MdDonutLarge } from "react-icons/md";
+import { history } from "./helpers/history";
+
+// import AuthVerify from "./common/AuthVerify";
+import EventBus from "./common/EventBus";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const App = () => {
+  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen((location) => {
+      dispatch(clearMessage()); // clear message when changing location
+    });
+  }, [dispatch]);
+
+  const logOut = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (currentUser) {
+      setShowModeratorBoard(currentUser.roles.includes("ROLE_MODERATOR"));
+      setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
+    } else {
+      setShowModeratorBoard(false);
+      setShowAdminBoard(false);
+    }
+
+    EventBus.on("logout", () => {
+      logOut();
+    });
+
+    return () => {
+      EventBus.remove("logout");
+    };
+  }, [currentUser, logOut]);
+
+  return (
+    <Router history={history}>
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">CRUEV Valle <BsHeartFill /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav ">
+              <Nav className="ml-auto">
+
+             
+
+                {showModeratorBoard && (
+                  <li className="nav-item">
+                    <Link to={"/mod"} className="nav-link">
+                      Moderator Board
+                    </Link>
+                  </li>
+                )}
+
+                {showAdminBoard && (
+                  <li className="nav-item">
+                    <Link to={"/admin"} className="nav-link">
+                      Admin Board
+                    </Link>
+                  </li>
+                )}
+                {currentUser && (
+                  <li className="nav-item">
+                    <Link to={"/user"} className="nav-link">
+                      User
+                    </Link>
+                  </li>
+                )}
+
+
+                {currentUser ? (
+                  <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <Link to={"/profile"} className="nav-link">
+                        {currentUser.username}
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <a href="/login" className="nav-link" onClick={logOut}>
+                        <MdDonutLarge/> LogOut
+                      </a>
+                    </li>
+                  </div>
+                ) : (
+                  <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <Link to={"/login"} className="nav-link">
+                      <MdDonutLarge/> Login
+                      </Link>
+                    </li>
+                  </div>
+                )}
+
+
+              </Nav>
+
+
+
+
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Login} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/user" component={BoardUser} />
+            <Route path="/mod" component={BoardModerator} />
+            <Route path="/admin" component={BoardAdmin} />
+          </Switch>
+        </div>
+
+        {/* <AuthVerify logOut={logOut}/> */}
+      </div>
+    </Router>
+  );
+};
+
+export default App;
